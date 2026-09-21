badaa6e5d09b2a1c71b0b6166804b4aef834361f
natgruzdeva <gruzdevanatalia1995@gmail.com>
2026-09-21 23:10:06 +0300

    Add main function for calculator

---
+++ HW2_gruzdeva/calculator.py
@@ -0,0 +1,34 @@
+def main():
+    #Ввод строки с математическим выражением, числа разделены пробелами
+    user_input = input("Введите выражение:")
+    
+    #Разбиваем выражение по пробелам на числа
+    elements = user_input.split()
+
+    #Переводим 1 и 3 элементы в числа
+    num1 = float(elements[0])
+    num2 = float(elements[2])
+    operator = elements[1]
+    
+    #Проверка на математическую операцию
+    if operator == '+':
+        output = add(num1, num2)
+    elif operator == '-':
+        output = subtraction(num1, num2)
+    elif operator == '*':
+        output = multiply(num1, num2)
+    elif operator == '/':
+        output = divide(num1, num2)
+    else:
+        print("Операция неизвестна данному калькулятору")
+        return
+
+    #Итоговый результат
+    print(output)
+
+#Функции членов команды №7 (add, subtraction, multiply, divide)
+
+
+
+if __name__ == "__main__":
+    main()
