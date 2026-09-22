bbefe8cb6cd99ea4663265ff1cea8b235fb4c22e
Darina Gavrilova <dzibrova@yandex.ru>
2026-09-22 14:26:50 +0300

    Add function divide

--- HW2_gruzdeva/calculator.py
+++ HW2_gruzdeva/calculator.py
@@ -1,33 +1,38 @@
 def main():
-    #Ввод строки с математическим выражением, числа разделены пробелами
+    # Ввод строки с математическим выражением, числа разделены пробелами
     user_input = input("Введите выражение:")
-    
-    #Разбиваем выражение по пробелам на числа
+
+    # Разбиваем выражение по пробелам на числа
     elements = user_input.split()
 
-    #Переводим 1 и 3 элементы в числа
+    # Переводим 1 и 3 элементы в числа
     num1 = float(elements[0])
     num2 = float(elements[2])
     operator = elements[1]
-    
-    #Проверка на математическую операцию
-    if operator == '+':
+
+    # Проверка на математическую операцию
+    if operator == "+":
         output = add(num1, num2)
-    elif operator == '-':
+    elif operator == "-":
         output = subtraction(num1, num2)
-    elif operator == '*':
+    elif operator == "*":
         output = multiply(num1, num2)
-    elif operator == '/':
+    elif operator == "/":
         output = divide(num1, num2)
     else:
         print("Операция неизвестна данному калькулятору")
         return
 
-    #Итоговый результат
+    # Итоговый результат
     print(output)
 
-#Функции членов команды №7 (add, subtraction, multiply, divide)
 
+# Функции членов команды №7 (add, subtraction, multiply, divide)
+
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
 
 
 if __name__ == "__main__":
