1cd86774219cdcf6833c2b9cd7ef8b6e1869b897
FernandDule <111347698+FernandDule@users.noreply.github.com>
2026-09-24 09:53:20 +0800

    Add main function and correct spelling in substraction

--- HW2_Podkopaeva/calculator.py
+++ HW2_Podkopaeva/calculator.py
@@ -2,5 +2,26 @@ def addition(a, b):
     return a + b
 
 
-def subtruction(a, b):
+def substruction(a, b):
     return a - b
+
+def main():
+    a, operation, b = input().split()
+    
+    if operation == '+':
+        result = addition(float(a), float(b))
+    elif operation == '-':
+        result = substruction(float(a), float(b))
+    elif operation == '*':
+        result = multiplication(float(a), float(b))
+    elif operation == '/':
+        if float(b) == 0:
+            print('Деление на 0')
+            return
+        result = division(float(a), float(b))
+    else:
+        print('Некорректное выражение')
+        return
+    
+    print(result)
+    return
