cf2bce828dd901e97a650439fc90ecfa1bfea153
garlic <garlic@123>
2026-09-25 21:11:33 +0500

    Add main function

--- calculator.py
+++ calculator.py
@@ -13,3 +13,23 @@ def multiply(a, b):
 
 def minus(a, b):
     return a - b
+
+def main():
+    user_input = input()
+
+    a, symbol, b = user_input.split()
+    
+    a = float(a)
+    b = float(b)
+
+    if symbol == "/":
+        result = divide(a, b)
+    elif symbol  == "+":
+        result = summa(a, b)
+    elif symbol == "*":
+        result = multiply(a, b)
+    elif symbol == "-":
+        result = minus(a, b)
+
+    print(result)
+main()
